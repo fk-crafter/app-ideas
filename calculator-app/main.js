44ce4feab99f95726b5cdae9600aa842e1425ea5
Fofo <127132293+fk-crafter@users.noreply.github.com>
2023-10-13 11:00:15 +0200

    Create main.js

---
+++ calculator-app/main.js
@@ -0,0 +1,28 @@
+let display = document.getElementById('display');
+
+function appendToDisplay(value) {
+    display.value += value;
+}
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
+
+document.addEventListener('keydown', function(event) {
+    const key = event.key;
+    if (key === 'Enter') {
+        calculate();
+    } else if (key === 'Backspace') {
+        display.value = display.value.slice(0, -1);
+    } else if (!isNaN(key)) {
+        appendToDisplay(key);
+    }
+});
