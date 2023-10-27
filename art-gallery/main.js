60d0b55d56458163d466cee3dc204c4fa5f7affb
Fofo <127132293+fk-crafter@users.noreply.github.com>
2023-10-27 17:19:05 +0200

    Create main.js

---
+++ art-gallery/main.js
@@ -0,0 +1,40 @@
+let currentImageIndex = 0;
+const images = ["img/image1.jpg", "img/image2.jpg", "img/image3.jpg", "img/image4.jpg", "img/image5.jpg"];
+const modalImage = document.getElementById('modalImage');
+const modal = document.getElementById('modal');
+
+modal.style.display = 'none';
+
+function openModal(imagePath) {
+  modal.style.display = 'flex';
+  modal.style.alignItems = 'center';
+  modalImage.src = imagePath;
+  currentImageIndex = images.indexOf(imagePath);
+
+  window.addEventListener('keydown', function(event) {
+    if (event.key === 'Escape') {
+      closeModal();
+    } else if (event.key === 'ArrowLeft') {
+      currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
+      modalImage.src = images[currentImageIndex];
+    } else if (event.key === 'ArrowRight') {
+      currentImageIndex = (currentImageIndex + 1) % images.length;
+      modalImage.src = images[currentImageIndex];
+    }
+  });
+}
+
+function closeModal() {
+  modal.style.display = 'none';
+  window.removeEventListener('keydown', function(event) {
+    if (event.key === 'Escape') {
+      closeModal();
+    } else if (event.key === 'ArrowLeft') {
+      currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
+      modalImage.src = images[currentImageIndex];
+    } else if (event.key === 'ArrowRight') {
+      currentImageIndex = (currentImageIndex + 1) % images.length;
+      modalImage.src = images[currentImageIndex];
+    }
+  });
+}
